c277a00b17abe8d049bc4af9e5018c772106e0d3
Carlos Dourado <110005935+carlosismaelad@users.noreply.github.com>
2023-03-08 14:55:26 -0300

    Add files via upload

---
+++ resful-api-nodejs/backend/src/router.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const tasksController = require("./controllers/tasksController");
+const tasksMiddleware = require("./middlewares/tasksMiddleware");
+
+router.get("/tasks", tasksController.getAll);
+router.post(
+  "/tasks",
+  tasksMiddleware.validateFieldTitle,
+  tasksController.createTask
+);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put(
+  "/tasks/:id",
+  tasksMiddleware.validateFieldTitle,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
+
+module.exports = router;
